e5f07be92cbbd49ba8563094b116b0c12975ec41
chizzy192 <122726814+chizzy192@users.noreply.github.com>
2025-08-25 11:02:13 +0100

    kk

--- Week Two/script.js
+++ Week Two/script.js
@@ -6,11 +6,7 @@ function convertTemperatureToCelcius(kelvin, fahrenheit) {
     const kelvinToCelcius = (kelvin - 273).toFixed(2);
     const fahrenheitToCelcius = (5/9 * (fahrenheit - 32)).toFixed(2);
 
-    if ((kelvin <= 0 || kelvin >= 0) && (fahrenheit <= 0 || fahrenheit >= 0)) {
-        console.log(`Celcius when kelvin is ${kelvin} = ${kelvinToCelcius} and fahrenheit is ${fahrenheit} = ${fahrenheitToCelcius}.`);
-    }else {
-        console.log(`not possible`);
-    }
+    console.log(`Celcius when kelvin is ${kelvin} = ${kelvinToCelcius} and fahrenheit is ${fahrenheit} = ${fahrenheitToCelcius}.`);
 }
 
 function convertTemperatureToKelvin(celcius, fahrenheit) {
@@ -18,11 +14,9 @@ function convertTemperatureToKelvin(celcius, fahrenheit) {
     const celciusToKelvin = (celcius + 273).toFixed(2);
     const fahrenheitToKelvin = (5/9 * (fahrenheit - 32) + 273).toFixed(2);
 
-    if ((celcius <= 0 || celcius >= 0) && (fahrenheit <=0 || fahrenheit >=0)) {
-        console.log(`kelvin when celcius is ${celcius} = ${celciusToKelvin} and fahrenheit is ${fahrenheit} = ${fahrenheitToKelvin}.`);
-    }else {
-        console.log(`not possible`);
-    }
+    console.log(`kelvin when celcius is ${celcius} = ${celciusToKelvin} and fahrenheit is ${fahrenheit} = ${fahrenheitToKelvin}.`);
+
+
 }
 
 function convertTemperatureToFahrenheit(celcius, kelvin) {
@@ -30,9 +24,8 @@ function convertTemperatureToFahrenheit(celcius, kelvin) {
     const celciustoFahrenheit = ((celcius * 9/5) + 32).toFixed(2);
     const kelvinToFahrenheit = (9/5 * (kelvin - 273) + 32).toFixed(2);
 
-    if ((celcius <= 0 || celcius >= 0) && (kelvin <=0 || kelvin >=0)) {
-        console.log(`Fahrenheit when celcius is ${celcius} = ${celciustoFahrenheit} and kelvin is ${kelvin} = ${kelvinToFahrenheit}.`);
-    }else {
-        console.log(`not possible`);
-    }
+    console.log(`Fahrenheit when celcius is ${celcius} = ${celciustoFahrenheit} and kelvin is ${kelvin} = ${kelvinToFahrenheit}.`);
+
 }
+
+convertTemperatureToFahrenheit(0, 7)
